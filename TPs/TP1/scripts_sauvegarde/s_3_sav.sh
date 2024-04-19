b6e1dabfdb49785328dc090f0962f95d4d4a7fea
mahmoud-illourmane <illourmanemahmoud@hotmail.com>
2024-04-19 13:30:16 +0000

    amélioration du code du script de sauvegarde.

--- TPs/TP1/scripts_sauvegarde/s_3_sav.sh
+++ TPs/TP1/scripts_sauvegarde/s_3_sav.sh
@@ -10,10 +10,19 @@
 # ./script_sauvegarde.sh mon_archive /chemin/vers/repertoire adresse_serveur login:password
 # Remplace mon_archive, /chemin/vers/repertoire, adresse_serveur, login, et password par les valeurs appropriées pour ta sauvegarde.
 
+C_G="\033[32m"
+C_R="\033[31m"
+C_Y="\033[33m"
+C_END="\033[00m"
+
+echo_() {
+    echo -e "$1$2$C_END"
+}
+
 # Vérification du nombre de paramètres
 if [ "$#" -ne 5 ]; then
-    echo -e "\e[31mUsage: $0 <nom_archive> <repertoire_a_sauvegarder> \"adresse_serveur_sauvegarde\" \"login\" \"password\" \e[0m"
-    exit 1
+  echo_ $C_R "Usage: $0 <nom_archive> <repertoire_a_sauvegarder> \"adresse_serveur_sauvegarde\" \"login\" \"password\""
+  exit 1
 fi
 
 # Attribution des paramètres à des variables
@@ -25,10 +34,10 @@ PASSWORD=$5
 
 # Création de l'archive avec tar
 if ! tar -czf "${NOM_ARCHIVE}.tar.gz" -C "$(dirname "$REPERTOIRE_A_SAUVEGARDER")" "$(basename "$REPERTOIRE_A_SAUVEGARDER")"; then
-  echo -e "\e[31mÉchec de la création de l'archive.\e[0m"
+  echo_ $C_R "Échec de la création de l'archive."
   exit 1
 fi
-echo -e "\e[32mL'archive a bien été créée.\e[0m"
+echo_ $C_G "L'archive a bien été créée."
 
 # Copie de l'archive via SFTP
 if ! sshpass -p "$PASSWORD" sftp "$LOGIN@$ADRESSE_SERVEUR" <<EOF
@@ -36,7 +45,7 @@ put "${NOM_ARCHIVE}.tar.gz" /home/$LOGIN/archives
 bye
 EOF
 then
-  echo -e "\e[31mÉchec de la copie de l'archive via SFTP.\e[0m"
+  echo_ $C_R "Échec de la copie de l'archive via SFTP."
   exit 1
 fi
 
@@ -44,5 +53,5 @@ if [ -f "${NOM_ARCHIVE}.tar.gz" ]; then
   rm -f "${NOM_ARCHIVE}.tar.gz"
 fi
 
-echo -e "\e[32mSauvegarde terminée : ${NOM_ARCHIVE}.tar.gz a été copié sur $ADRESSE_SERVEUR\e[0m"
+echo_ $C_G "Sauvegarde terminée : ${NOM_ARCHIVE}.tar.gz a été copié sur $ADRESSE_SERVEUR"
 exit 0
---
+++ TPs/TP1/sync/test_func_files/delete_olds_files.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Définition des codes de couleur
+C_G="\033[32m"
+C_R="\033[31m"
+C_Y="\033[33m"
+C_END="\033[00m"
+
+# Fonction pour afficher les messages avec la couleur appropriée
+echo_() {
+    echo -e "$1$2$C_END"
+}
+
+# Demande à l'utilisateur combien de fichiers entrer
+echo_ $C_Y "Combien de fichiers voulez-vous entrer ?"
+read num_files
+if ! [[ "$num_files" =~ ^[0-9]+$ ]]; then
+    echo_ $C_R "Erreur : Vous avez entré une valeur qui n'est pas un entier."
+    exit 1
+fi
+
+# Initialisation de la liste des fichiers
+files=()
+
+# Lecture des noms de fichiers que l'utilisateur souhaite entrer
+for (( i = 1; i <= num_files; i++ )); do
+    echo "Entrez le nom du fichier $i :"
+    read file_name
+    # Ajout du nom du fichier à la liste
+    files+=("$file_name")
+done
+
+# Vérification que les fichiers entrés existent
+for file in "${files[@]}"; do
+    if [ ! -e "$file" ]; then
+        echo_ $C_R "Erreur : Le fichier '$file' n'existe pas."
+        exit 1
+    fi
+done
+
+ls -t "${files[@]}" > tmp_file_list.txt
+if [ $? -ne 0 ]; then
+    echo_ $C_R "Erreur lors de l'exécution de la commande ls -t."
+    exit 1
+fi
+
+# Vérification que le fichier tmp_file_list.txt n'est pas vide
+if [ ! -s tmp_file_list.txt ]; then
+    echo_ $C_R "Erreur : tmp_file_list.txt est vide ou inexistant."
+    exit 1
+fi
+
+# Lecture du fichier le plus récent
+read most_recent_file < tmp_file_list.txt
+
+# Suppression des autres fichiers sauf le plus récent
+for file in "${files[@]}"; do
+    if [ "$file" != "$most_recent_file" ]; then
+        rm "$file"
+        if [ $? -ne 0 ]; then
+            echo_ $C_R "Erreur : Impossible de supprimer le fichier '$file'."
+        else
+            echo_ $C_Y "Fichier '$file' supprimé avec succès."
+        fi
+    fi
+done
+
+# Suppression du fichier temporaire
+rm tmp_file_list.txt
+if [ $? -ne 0 ]; then
+    echo_ $C_R "Erreur lors de la suppression du fichier tmp."
+    exit 1
+fi
+
+# Afficher le nom du fichier le plus récent
+echo_ $C_G "Le fichier le plus récent est : $most_recent_file"
+
+exit 0
